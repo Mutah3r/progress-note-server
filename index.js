3990b42f8ed74a87fd1106605735d5f4ed577887
Mutah3r <evil.programmer.stephen@gmail.com>
2023-06-04 18:01:58 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -2,15 +2,63 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 
 // middle-wares
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('ProgressNote Server is in progress....')
-})
+// MongoDB URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@porfolioprojects.vkb3mrm.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const tasksCollection = client.db("ProgressNote").collection("tasks");
+
+        app.get('/', (req, res) => {
+            res.send('ProgressNote Server is in progress....')
+        });
+
+
+        app.post('/addTask', async (req, res) => {
+            const task = req.body;
+
+            console.log(task);
+
+            try{
+                // const result = await tasksCollection.insertOne(task);
+                // res.send(result);
+            }
+            catch(error){
+
+            }
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`ProgressNote app listening on port ${port}`)
+    console.log(`ProgressNote app listening on port ${port}`)
 })
